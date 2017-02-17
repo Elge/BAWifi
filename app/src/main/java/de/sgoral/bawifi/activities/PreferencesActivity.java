7504819bcc07e3ec9e98f122076fa58c0df52c74
Elge <info@sebastian-goral.de>
2017-02-17 11:29:00 +0100

    Added reset preferences button

--- app/src/main/java/de/sgoral/bawifi/activities/PreferencesActivity.java
+++ app/src/main/java/de/sgoral/bawifi/activities/PreferencesActivity.java
@@ -2,10 +2,15 @@ package de.sgoral.bawifi.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.SwitchPreference;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
 import de.sgoral.bawifi.R;
 import de.sgoral.bawifi.fragments.ClearLogFragment;
+import de.sgoral.bawifi.util.Logger;
 import de.sgoral.bawifi.util.PreferencesUtil;
 
 /**
@@ -23,5 +28,9 @@ public class PreferencesActivity extends Activity {
         PreferencesUtil.getInstance(this).clearLogEntries();
         ((ClearLogFragment) this.getFragmentManager().findFragmentById(R.id.fragment_clear_log))
                 .updateButtonVisibility();
+    }
+
+    public void onResetPreferencesClicked(View view) {
+        PreferencesUtil.getInstance(this).resetPreferences();
     }
 }
